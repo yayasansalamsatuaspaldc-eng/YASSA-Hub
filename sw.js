eaff5757cc82c7fdabe4992eb1997bde63f7948a
yayasansalamsatuaspaldc-eng <yayasansalamsatuaspal.dc@gmail.com>
2026-08-09 08:04:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // bisa kebuka meski koneksi lagi jelek. Konten di dalam iframe (Kotak & Donasi,
 // Absensi, AWG, Penerima Manfaat) TETAP butuh internet karena itu app terpisah.
 
-const CACHE_NAME = "yassa-hub-shell-v6";
+const CACHE_NAME = "yassa-hub-shell-v7";
 const SHELL_FILES = [
   "./",
   "./index.html",
